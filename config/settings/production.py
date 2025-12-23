b68171b5eda743d34e852e8f86e494fae71192c0
Artjom Vassiljev <artjom.vassiljev@montonio.com>
2025-12-23 21:30:22 +0200

    Add CSRF origins

--- config/settings/production.py
+++ config/settings/production.py
@@ -7,3 +7,10 @@ from .base import *
 DEBUG = os.environ.get('DEBUG', False) == 'True'
 
 ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS', '').split(',')
+
+# CSRF settings
+CSRF_TRUSTED_ORIGINS = [
+    origin.strip()
+    for origin in os.environ.get('CSRF_TRUSTED_ORIGINS', '').split(',')
+    if origin.strip()
+]
